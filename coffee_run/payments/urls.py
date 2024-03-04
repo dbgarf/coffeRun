69bb2d49756d1609165f0d74d75191b7ec85f908
Daniel Garfield <dbgarf@gmail.com>
2024-03-04 11:36:45 -0700

    basically working

--- coffee_run/payments/urls.py
+++ coffee_run/payments/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("users/create/", views.UserCreateView.as_view(), name="user_create"),
     path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="user_update"),
     path("users/<int:pk>/delete/", views.UserDeleteView.as_view(), name="user_delete"),
+    path("group_orders/", views.list_group_orders, name="group_order_list"),
+    path("group_orders/create/", views.create_group_order, name="group_order_create"),
+    path("group_orders/<int:pk>/detail/", views.detail_group_order, name="group_order_detail")
 ]
--- coffee_run/payments/views.py
+++ coffee_run/payments/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
 from .models import User, GroupOrder, GROUP_ORDER_STATUS, OrderItem
 
+import json
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Index Page for CoffeeRun")
+    context = {}
+    return render(request, "payments/index.html", context)
 
 def list_users(request: HttpRequest) -> HttpResponse:
     users = User.objects.all()
@@ -19,11 +20,49 @@ def list_users(request: HttpRequest) -> HttpResponse:
 class UserCreateView(CreateView):
     model = User
     fields= ['name']
+    success_url = reverse_lazy("user_list")
 
 class UserUpdateView(UpdateView):
     model = User
     fields = ['name']
+    success_url = reverse_lazy("user_list")
 
 class UserDeleteView(DeleteView):
     model = User
-    success_url = reverse_lazy("list_users")
+    success_url = reverse_lazy("user_list")
+
+def list_group_orders(request: HttpRequest) -> HttpResponse:
+    group_orders = GroupOrder.objects.all()
+    context = {'group_orders': group_orders}
+    return render(request, "payments/group_order_list.html", context)
+
+def detail_group_order(request: HttpRequest, pk: int) -> HttpResponse:
+    group_order = GroupOrder.objects.get(id=pk)
+    context =  {'group_order': group_order, 'order_items': group_order.orderitem_set.all()}
+    return render(request, "payments/group_order_detail.html", context)
+
+def create_group_order(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        users = []
+        for user in User.objects.all():
+            users.append({
+                'id': user.id,
+                'name': user.name
+            })
+        context = {'users': users}
+        return render(request, "payments/group_order_create.html", context)
+    
+    if request.method == "PUT":
+        json_payload = json.loads(request.body)
+        group_order = GroupOrder.objects.create()
+        for order_item in json_payload:
+            user = User.objects.get(pk=order_item['user'])
+            OrderItem.objects.create(
+                name=order_item['name'],
+                price=order_item['price'],
+                ordered_by=user,
+                group_order=group_order
+            )
+        group_order.complete_order()
+        detail_url = f"/group_orders/{group_order.pk}/detail/"
+        return HttpResponse(detail_url)
